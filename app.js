fa8b4fd649b765b4d16b87da3a8e0036200b5247
Hernán Marrapodi <69157896+marrahenzo@users.noreply.github.com>
2023-01-09 23:40:02 -0300

    Part 7 complete

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -23,6 +25,8 @@ db.on('error', console.error.bind(console, 'MongoDB connecion error: '));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
+app.use(compression());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
